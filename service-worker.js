476594ffe9b33e79ac2c857e475dc2ab93a7859c
kico1980 <kiefer_martin@web.de>
2024-11-11 16:16:39 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,3 @@
-
-//Service worker
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(function(error) {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
-
-//<- service worker
-
-
-
 function würfeln() {
 }
 
